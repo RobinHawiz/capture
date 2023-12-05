db60ec525ca0a60ab888d92fcd64e054d346bf41
RobinH <Robin.Hawiz.6670@student.uu.se>
2023-12-05 18:10:12 +0100

    Faq animation

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,10 +1,15 @@
+//useState
+import { useState } from "react";
 import styled from "styled-components";
 import { Section } from "../styles";
 //Animations
-import { motion } from "framer-motion";
+import { motion, LayoutGroup } from "framer-motion";
 import { pageAnimation } from "../Animation";
+//Components
+import Toggle from "./Toggle";
 
 const FaqSection = () => {
+  const [faqToggle, setFaqToggle] = useState(false);
   return (
     <Faq
       variants={pageAnimation}
@@ -15,56 +20,58 @@ const FaqSection = () => {
       <h2>
         Any questions? <span>FAQ</span>
       </h2>
-      <div className="question">
-        <h4>How do I start?</h4>
-        <div className="answer">
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,
-            mollitia.
-          </p>
-        </div>
-        <div className="faq-line"></div>
-      </div>
-      <div className="question">
-        <h4>Daily Schedule</h4>
-        <div className="answer">
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,
-            mollitia.
-          </p>
-        </div>
-        <div className="faq-line"></div>
-      </div>
-      <div className="question">
-        <h4>Different Payment Methods</h4>
-        <div className="answer">
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,
-            mollitia.
-          </p>
-        </div>
-        <div className="faq-line"></div>
-      </div>
-      <div className="question">
-        <h4>What Services Do You Offer?</h4>
-        <div className="answer">
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,
-            mollitia.
-          </p>
-        </div>
-        <div className="faq-line"></div>
-      </div>
+      <LayoutGroup>
+        <Toggle title="How do I start?">
+          <div className="question">
+            <div className="answer">
+              <p>Lorem ipsum dolor sit amet.</p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,
+                mollitia.
+              </p>
+            </div>
+          </div>
+        </Toggle>
+        <Toggle title="Daily Schedule">
+          <div className="question">
+            <div className="answer">
+              <p>Lorem ipsum dolor sit amet.</p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,
+                mollitia.
+              </p>
+            </div>
+          </div>
+        </Toggle>
+        <Toggle title="Different Payment Methods">
+          <div className="question">
+            <div className="answer">
+              <p>Lorem ipsum dolor sit amet.</p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,
+                mollitia.
+              </p>
+            </div>
+          </div>
+        </Toggle>
+        <Toggle title="What Services Do You Offer?">
+          <div className="question">
+            <div className="answer">
+              <p>Lorem ipsum dolor sit amet.</p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,
+                mollitia.
+              </p>
+            </div>
+          </div>
+        </Toggle>
+      </LayoutGroup>
     </Faq>
   );
 };
 
 //Styled components
-const Faq = styled(motion.Section)`
+const Faq = styled(Section)`
   display: block;
   span {
     display: block;
