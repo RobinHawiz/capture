a2a481ea9a719a6161470021902dcc9bfb956e3f
RobinH <Robin.Hawiz.6670@student.uu.se>
2023-12-05 20:08:46 +0100

    Remove console.log

--- src/components/useScroll.js
+++ src/components/useScroll.js
@@ -8,14 +8,10 @@ import { useAnimationControls } from "framer-motion";
 export const UseScroll = (treshold) => {
   const controls = useAnimationControls();
   const [element, view] = useInView({ threshold: treshold });
-  console.log("UseScroll");
-  console.log(element);
   useEffect(() => {
     if (view) {
-      console.log("show");
       controls.start("show");
     } else {
-      console.log("hidden");
       controls.start("hidden");
     }
   }, [element, view]);
